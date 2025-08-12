a26d4c44f44c7e99bda2ca63e0ccb47526490e55
Juan Pablo de León Miranda <JuanPablodeLeon>
2025-08-12 14:27:36 -0600

    CML corregido clase nodo creada

---
+++ Nodo/Nodo.h
@@ -0,0 +1,14 @@
+//
+// Created by juanp on 12/08/2025.
+//
+
+#ifndef EDD_PRACTICA1_2020311101_NODO_H
+#define EDD_PRACTICA1_2020311101_NODO_H
+
+
+class Nodo
+{
+};
+
+
+#endif //EDD_PRACTICA1_2020311101_NODO_H
